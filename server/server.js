f36526ab4cfab6027e05bf9a2950392f94916f7a
ofertaramadani <oferte.ramadani@student.uni-pr.edu>
2023-03-27 15:57:07 +0200

    Database config for testing

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ app.get("/api", (req,res) => {
 })
 
 
+app.get("/login", (req,res) => {
+    res.json({"test" : ["test1","test2","test3"]})
+})
+
 app.listen(5000, ()=>{
     console.log("listening on http://localhost:5000")
 })
