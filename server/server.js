aff568c321362b0f052e707e15bbd5298fe106cc
yllberisha <94076526+yllberishastudent@users.noreply.github.com>
2023-04-01 17:19:30 +0200

    Fixed Sign up endpoint
    Fixed Sign up endpoint
    Encryption + Validation Needed!

--- server/server.js
+++ server/server.js
@@ -1,74 +1,69 @@
-
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const Sequelize = require('sequelize');
+const express = require("express");
+const bodyParser = require("body-parser");
+const Sequelize = require("sequelize");
 
 // Initialize Sequelize with database credentials
-const sequelize = new Sequelize('library', 'root', 'tiktak123', {
-  host: 'localhost',
-  port:3307,
-  dialect: 'mysql'
+const sequelize = new Sequelize("library", "root", "admin", {
+  host: "localhost",
+  port: 3306,
+  dialect: "mysql",
 });
 
 // Define the Users table
-const User = sequelize.define('user', {
+const User = sequelize.define(
+  "users",
+  {
     username: {
       type: Sequelize.STRING,
-      allowNull: false
+      allowNull: false,
     },
     email: {
       type: Sequelize.STRING,
       allowNull: false,
-      unique: true
+      unique: true,
     },
     phone_number: {
       type: Sequelize.STRING,
       allowNull: false,
-      unique: true
+      unique: true,
     },
     password: {
       type: Sequelize.STRING,
-      allowNull: false
+      allowNull: false,
     },
-    confirm_password: {
-      type: Sequelize.STRING,
-      allowNull: false
-    }
-  }, { 
-    timestamps: false // disable the creation of createdAt and updatedAt fields
-  });
-
+  },
+  {
+    timestamps: false, // disable the creation of createdAt and updatedAt fields
+  }
+);
 
 // Initialize the app
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Define a POST endpoint to create a new user
-app.post('/signup', async (req, res) => {
+app.post("/signup", async (req, res) => {
   try {
-    const { username, email, phone_number, password, confirm_password } = req.body;
-    const user = await User.create({ username, email, phone_number, password, confirm_password });
+    const { username, password, email, phone_number } = req.body;
+    const user = await User.create({ username, password, email, phone_number });
     res.status(201).json({ user });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Unable to create user' });
+    res.status(500).json({ error: "Unable to create user" });
   }
 });
-
 // Define a POST endpoint to login
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ where: { username, password } });
     if (!user) {
-      return res.status(401).json({ error: 'Invalid username or password' });
+      return res.status(401).json({ error: "Invalid username or password" });
     }
     res.status(200).json({ user });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Unable to login' });
+    res.status(500).json({ error: "Unable to login" });
   }
 });
 
