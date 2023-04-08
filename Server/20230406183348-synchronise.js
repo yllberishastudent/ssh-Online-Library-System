8336b4b81fe7f7fee8133997583d7bab7e9ee9ea
Sueda Guri <suedagurii@gmail.com>
2023-04-09 00:46:30 +0200

    Node_modules changes

---
+++ Server/20230406183348-synchronise.js
@@ -0,0 +1,23 @@
+const db = require("./models");
+
+// Synchronize all models with the database in a specific order
+db.sequelize
+  .sync({ force: false })
+  .then(() => {
+    return db.User.sync();
+  })
+  .then(() => {
+    return db.Book.sync();
+  })
+  .then(() => {
+    return db.Transaction.sync();
+  })
+  .then(() => {
+    return db.Favorite.sync();
+  })
+  .then(() => {
+    console.log("All models were synchronized successfully.");
+  })
+  .catch((error) => {
+    console.error("An error occurred while synchronizing the models:", error);
+  });
