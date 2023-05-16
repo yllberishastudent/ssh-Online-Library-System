585d19f9cb95f43ebc8f8c0aff8cd126dd784692
selvijebucolli1 <selvije.bucolli87@gmail.com>
2023-05-16 16:27:46 +0200

    Added FAQ input from user

--- client/src/component/user/FAQ.js
+++ client/src/component/user/FAQ.js
@@ -1,20 +1,46 @@
 import React, { useState, useEffect } from "react";
 import "./style/FAQ.css";
+import jwtDecode from "jwt-decode";
 import axios from "axios";
+
 
 function FAQ() {
   const [faq, setFaq] = useState([]);
+  const [question, setQuestion] = useState("");
+  const token = localStorage.getItem("token");
+  const decodedToken=jwtDecode(token);
+  const userId=decodedToken.id;
 
   useEffect(() => {
-    axios
-      .get("http://localhost:5001/faq") // Replace with the API endpoint to fetch FAQ data
-      .then((response) => {
-        setFaq(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    fetchFAQs();
   }, []);
+
+  const fetchFAQs = async () => {
+    try {
+      const response = await axios.get("http://localhost:5001/faq?limit=10");
+      setFaq(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleQuestionSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:5001/faq", {
+        question: question,
+        answer: "Please wait for your answer...", 
+        status: "unanswered", 
+        user_id: userId, 
+      });
+
+      const newFaq = response.data;
+      setFaq([newFaq, ...faq.slice(0, 9)]);
+      setQuestion("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="container">
@@ -22,7 +48,7 @@ function FAQ() {
         <div className="books-container">
           <div>
             <h3 id="faq" className="FAQ__title">
-              FREQUENTLY ASKED QUESTIONS
+              RECENTLY ASKED QUESTIONS
             </h3>
             <div className="FAQ-grid">
               {faq.map((item, index) => (
@@ -33,6 +59,20 @@ function FAQ() {
                   </details>
                 </div>
               ))}
+           
+
+            <form onSubmit={handleQuestionSubmit} className="question-form">
+              <input
+                type="text"
+                value={question}
+                onChange={(e) => setQuestion(e.target.value)}
+                placeholder="Enter your question"
+                className="question-input"
+              />
+              <button type="submit" className="submit-btn">
+                Submit
+              </button>
+            </form>
             </div>
           </div>
         </div>
