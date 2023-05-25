5133b7eeb4a0f0eefb41879b9e7ca5004f677493
vanesaaliu <94717763+vanesaaliu@users.noreply.github.com>
2023-05-25 20:01:05 +0200

    added functional delete buttons

--- client/src/component/user/UserHistoryPage.js
+++ client/src/component/user/UserHistoryPage.js
@@ -25,29 +25,78 @@ const UserHistoryPage = ({ userId }) => {
     fetchUserHistory();
   }, [userId]);
 
+  const handleDeleteBook = async (bookId) => {
+    try {
+      await axios.delete(`http://localhost:5001/history/user/${bookId}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-Type": "application/json",
+          "X-Requested-With": "XMLHttpRequest",
+          "Custom-Header": "value",
+        },
+      });
+      // Remove the book from the reading history
+      setReadingHistory((prevHistory) =>
+        prevHistory.filter((entry) => entry.book_id !== bookId)
+      );
+      console.log("Book deleted successfully.");
+    } catch (error) {
+      console.error("Error deleting book:", error);
+    }
+  };
+
+  const handleDeleteAll = async () => {
+    try {
+      await axios.delete("http://localhost:5001/history/all/user", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-Type": "application/json",
+          "X-Requested-With": "XMLHttpRequest",
+          "Custom-Header": "value",
+        },
+      });
+      // Clear the reading history
+      setReadingHistory([]);
+      console.log("All books deleted successfully.");
+    } catch (error) {
+      console.error("Error deleting books:", error);
+    }
+  };
+
   return (
     <div>
-      <h2>User History</h2>
+      <h2 className="user-history">User History</h2>
+      <button className="delete-all-btn" onClick={handleDeleteAll}>
+        Delete All
+      </button>
       {readingHistory.length > 0 ? (
         <ul>
           {readingHistory.map((entry) => (
-            <section className="book-details" key={entry.history_id}>
+            <section className="book-details-history" key={entry.book_id}>
               <div className="container">
-                <div className="book-details-content grid">
+                <div className="book-history-content grid">
                   <div className="book-details-img">
                     <img
                       src={`/images/${entry.Book.cover_image_url}`}
                       alt={entry.Book.title}
                     />
                   </div>
-                  <div className="book-details-info">
-                    <div className="book-details-item title">
+                  <div className="book-history-info">
+                    <div className="book-history-item title">
                       <span className="fw-6 fs-24">{entry.Book.title}</span>
                     </div>
-                    <div className="book-details-item description">
+                    <div className="book-history-item description">
                       <span className="fw-6 fs-24">
                         {entry.Book.description}
                       </span>
+                    </div>
+                    <div>
+                      <button
+                        className="delete-btn"
+                        onClick={() => handleDeleteBook(entry.book_id)}
+                      >
+                        Delete
+                      </button>
                     </div>
                   </div>
                 </div>
